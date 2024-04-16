2b41c941640528124ca1ba109a1e74f38e1c4b9d
Willian dos Anjos <Willian2006.com@gmail.com>
2024-04-16 08:14:33 -0300

    Lista 07 reajeitada ahhhhhhhhhhhhhhhhhh

--- lista07/src/ifsc/MainProduto.java
+++ lista07/src/ifsc/MainProduto.java
@@ -73,7 +73,10 @@ public class MainProduto {
 			System.out.println(desktop.getFabricante());
 			System.out.println(desktop.getGamer());
 			System.out.println(desktop.getCodigodebarras());
-			System.out.println(desktop.getPecas());
+			//System.out.println(desktop.getPecas());
+			for (String peca : desktop.getPecas()) {
+				System.out.println(peca);
+			}
 		}
 		System.out.println("");
 		System.out.println("Os celulares sao:");
