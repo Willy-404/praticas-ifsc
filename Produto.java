0c1d44f5b77c983cf99177425dce28fdf068b815
Willian dos Anjos <Willian2006.com@gmail.com>
2024-04-16 08:00:53 -0300

    Lista 07 Compleeeeeeeeeeeeeeta

---
+++ Produto.java
@@ -0,0 +1,22 @@
+package ifsc;
+
+public class Produto {
+	
+	private String fabricante;
+	private Long codigodebarras;
+	
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public Long getCodigodebarras() {
+		return codigodebarras;
+	}
+	public void setCodigodebarras(Long codigodebarras) {
+		this.codigodebarras = codigodebarras;
+	}
+
+	
+}
