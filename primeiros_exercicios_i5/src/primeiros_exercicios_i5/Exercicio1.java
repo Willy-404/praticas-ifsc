1f788f0b3c7ddf4ad83e2b62828d3cea454a1234
Willian dos Anjos <165198824+Willy-404@users.noreply.github.com>
2024-09-13 09:10:08 -0300

    Add files via upload
    Vamo o caraio

--- primeiros_exercicios_i5/src/primeiros_exercicios_i5/Exercicio1.java
+++ primeiros_exercicios_i5/src/primeiros_exercicios_i5/Exercicio1.java
@@ -1,10 +1,12 @@
 package primeiros_exercicios_i5;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner leitura = new Scanner(System.in);
+		float numero = Float.valueOf(leitura.nextLine());
 	}
 
 }
